c114421504af0bef9bf7eb91e18ea3bc1ee4a7dd
Justin Ware <justin@justinware.me.uk>
2024-07-23 21:29:16 +0100

    Camera, collisions, basic interaction.
    Added camera to track hero around bigger main screen.
    Hero has collision detection and spots new villain.
    Simple dialog is shown.

--- Hero.cs
+++ Hero.cs
@@ -1,10 +1,18 @@
 using Godot;
 using System;
 
+/// <summary>
+/// A class representing the hero. 
+/// </summary>
 public partial class Hero : CharacterBody2D
 {
 	public const float SPEED = 400.0f;
+	// Whether the hero is allowed to move
+	public bool _moving = true;
 
+	/// <summary>
+	/// Get any input keys being used and update hero's velocity.
+	/// </summary>
 	private void get_input()
 	{
 		var input_direction = Input.GetVector("left", "right", "up", "down");
@@ -12,16 +20,47 @@ public partial class Hero : CharacterBody2D
 		Velocity = input_direction * SPEED;
 	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-
-		Position = new Vector2(200, 200);
-    }
-
+	/// <summary>
+	/// Override the physics of the hero. The hero moves with the velocity determind by the arrow keys.
+	/// </summary>
+	/// <param name="delta">Unused.</param>
     public override void _PhysicsProcess(double delta)
 	{
 		get_input();
-		MoveAndSlide();
+
+		// Only move if the dialog isn't open
+		if (_moving)
+		{
+			MoveAndSlide();
+		}
+	}
+
+	/// <summary>
+	/// The hero has collided with another node on the screen. 
+	/// </summary>
+	/// <param name="other"></param>
+	private void _on_area_2d_body_entered(Node2D other)
+	{
+		// Only care about the villain
+		if ((other != null) && (other.Name == "Villain"))
+		{
+			// Stop moving
+			_moving = false;
+
+			// Simple popup to greet the villain
+			var dialog = new AcceptDialog();
+			dialog.DialogText = "So we meet again!";
+			dialog.Title = "Greetings";
+            dialog.Confirmed += () =>
+			{
+				// Remove the dialog
+				dialog.QueueFree();
+
+				// We can move again
+				_moving = true;
+			};
+			AddChild(dialog);
+			dialog.PopupCentered();
+		}
 	}
 }
